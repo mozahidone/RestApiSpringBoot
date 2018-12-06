39ffbedd180abb798a6d84223972918984835edb
mozahid <mozahidone@gmail.com>
2018-12-06 22:05:12 +0600

    Use RestController annotation

--- src/main/java/com/teksoi/restapi/controller/WelcomeController.java
+++ src/main/java/com/teksoi/restapi/controller/WelcomeController.java
@@ -1,18 +1,16 @@
 package com.teksoi.restapi.controller;
 
 import com.teksoi.restapi.beans.Welcome;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WelcomeController {
 
 	private static final String welcomemsg = "Welcome Mr. %s!";
 
     @GetMapping("/welcome/user")
-    @ResponseBody
     public Welcome welcomeUser(@RequestParam(name="name", required=false, defaultValue="Zayed") String name) {
         return new Welcome(String.format(welcomemsg, name));
     }
