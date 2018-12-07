c371f0716dbc6213d75899a6a34e01a1996f1a82
mozahid <mozahidone@gmail.com>
2018-12-07 23:46:34 +0600

    Deploy a Spring Boot war into a Tomcat Server

--- src/main/java/com/teksoi/restapi/SpringBootRestApplication.java
+++ src/main/java/com/teksoi/restapi/SpringBootRestApplication.java
@@ -2,9 +2,10 @@ package com.teksoi.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = {"com.teksoi.restapi"})
-public class SpringBootRestApplication {
+public class SpringBootRestApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApplication.class, args);
